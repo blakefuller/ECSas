7c6cb55ca05225cafa02129c11287c9909cb27e0
Blake <bfuller3134@gmail.com>
2020-06-04 23:47:36 -0700

    categorized anncs in admin view

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -14,8 +14,14 @@ const styles = {
 }
 
 export class admin extends Component {
+  // set state
   state = {
     anncs: null,
+    ecsAnncs: [],
+    ecsEvents: [],
+    regAdv: [],
+    ccc: [],
+    offCampus: [],
     loading: true,
     authError: false,
     openBar: false
@@ -31,12 +37,49 @@ export class admin extends Component {
     });
   };
 
+  // function to render each announcement category
+  renderCategory = (category) => {
+    let rendered = this.state[category] ? (
+      this.state[category].map((annc) => (
+        <Grid item key={annc.id}>
+          <Annc annc={annc} />
+        </Grid>
+      ))
+    ) : (
+      <p>Loading...</p>
+    );
+    return rendered;
+  }
+
   componentDidMount() {
     axios
       .get("/anncs", {
         headers: { Authorization: `${localStorage.FBIdToken}` },
       })
       .then((res) => {
+        // map announcement to correct category
+        res.data.forEach(annc => {
+          switch(annc.category) {
+            case 'SPU ECS announcements':
+              this.state.ecsAnncs.push(annc);
+              break
+            case 'SPU ECS events':
+              this.state.ecsEvents.push(annc)
+              break
+            case 'SPU ECS events':
+              this.state.ecsEvents.push(annc)
+              break
+            case 'Registration/Advising':
+              this.state.regAdv.push(annc)
+              break
+            case 'SPU Center for Career and Calling':
+              this.state.ccc.push(annc)
+              break
+            case 'Off campus non-SPU events':
+              this.state.offCampus.push(annc)
+              break
+          }
+        });
         this.setState({
           anncs: res.data,
         });
@@ -50,20 +93,48 @@ export class admin extends Component {
   }
   render() {
     const { classes } = this.props;
-    let currentAnncs = this.state.anncs ? (
-      this.state.anncs.map((annc) => (
-        <Grid item key={annc.id}>
-          <Annc annc={annc} />
-        </Grid>
-      ))
-    ) : (
-      <p>Loading Announcements...</p>
-    );
+
+    let currentAnncs = this.renderCategory('anncs')
+    let ecsAnncs = this.renderCategory('ecsAnncs')
+    let ecsEvents = this.renderCategory('ecsEvents')
+    let regAdv = this.renderCategory('regAdv')
+    let ccc = this.renderCategory('ccc')
+    let offCampus = this.renderCategory('offCampus')
+
     return (
       <div>
+        <h1>ECS announcements</h1>
         <Grid container spacing={8}>
           <Grid item sm={8} xs={12}>
-            {currentAnncs}
+            {ecsAnncs}
+          </Grid>
+        </Grid>
+
+        <h1>ECS events</h1>
+        <Grid container spacing={8}>
+          <Grid item sm={8} xs={12}>
+            {ecsEvents}
+          </Grid>
+        </Grid>
+
+        <h1>Registration/Advising</h1>
+        <Grid container spacing={8}>
+          <Grid item sm={8} xs={12}>
+            {regAdv}
+          </Grid>
+        </Grid>
+
+        <h1>Center for Career and Calling</h1>
+        <Grid container spacing={8}>
+          <Grid item sm={8} xs={12}>
+            {ccc}
+          </Grid>
+        </Grid>
+
+        <h1>Off-campus events</h1>
+        <Grid container spacing={8}>
+          <Grid item sm={8} xs={12}>
+            {offCampus}
           </Grid>
         </Grid>
 
